4ae1d2c5f59417e9ada426d58dc100e3395c7088
jaytala <talavera.jrw@gmail.com>
2022-04-07 22:20:59 -0700

    Connecting to answers db

--- api/checking_service.py
+++ api/checking_service.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from utils import start_connection
 
 app = FastAPI()
 
+# connect to answers database
+conn = start_connection(2)
+c = conn.cursor()
 
 @app.get("/")
 async def root():
