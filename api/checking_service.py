6199802a5a255d1469e41f7df0b181afe441b1c2
jaytala <talavera.jrw@gmail.com>
2022-04-07 23:47:46 -0700

    Word of the day randomly selected from answers db

--- api/checking_service.py
+++ api/checking_service.py
@@ -7,6 +7,10 @@ app = FastAPI()
 conn = start_connection(2)
 c = conn.cursor()
 
+# select word of the day
+c.execute("SELECT * FROM answers ORDER BY RANDOM() LIMIT 1")
+wordOTD = c.fetchone()[1]
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"word of the day": wordOTD}
