c8249c7f821af6729ab946c763f71e2fc6d9b684
super-rogatory <chukwudi10@gmail.com>
2022-04-08 20:01:51 -0700

    feature: change the answer for future games

--- api/checking_service.py
+++ api/checking_service.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from utils import start_connection
 from validation_service import check_word
+import random
 
 app = FastAPI()
 
@@ -8,23 +9,29 @@ app = FastAPI()
 conn = start_connection(2)
 c = conn.cursor()
 
-# select word of the day
-# c.execute("SELECT * FROM answers ORDER BY RANDOM() LIMIT 1")
-# wordOTD = c.fetchone()[1]
-wordOTD = "tread"
+# (cur_Word, id, value) in database
+CUR_WORD_STATUS = 0
+ID = 1
+WORD = 2
 
 
 @app.post("/checking/checkanswer")
 async def check_answer(answer: str):
+    # grab word of the day
+    c.execute("SELECT * FROM answers WHERE cur_word=1")
+    wordOTD = c.fetchone()[WORD]
+
     # use validation function to check if word is valid.
     cw = await check_word(answer)
-    if not cw["isValidWord"]:
+    if not cw["isValidWord"] and answer != wordOTD:
         raise HTTPException(status_code=400, detail="Word is invalid.")
+
     # Word is valid from this point on. | In production replace object with a small list [1,2,3]
     statuses = {1: "absent", 2: "present", 3: "correct"}
     word_status = []  # list of objects. [{ "t", "present"}]
     index = 0
     isCorrect = False
+
     if answer == wordOTD:
         isCorrect = True
     else:
@@ -39,9 +46,29 @@ async def check_answer(answer: str):
     return {"answerResults": "Correct" if isCorrect else word_status}
 
 
-# @app.post("/checking/changeanswer"):
-# async def change_answer(answer: str):
-#     # use validation function to check if word is valid.
-#     cw = await check_word(answer)
-#     if not cw["isValidWord"]:
-#         raise HTTPException(status_code=400, detail="Word is invalid.")
+@app.put("/checking/changeanswer")
+async def change_answer():
+    # find maxId for random function
+    c.execute("SELECT id FROM answers ORDER BY id DESC LIMIT 1")
+    maxId = c.fetchone()[0]  # rip value from query
+    randomId = random.randint(1, maxId)
+
+    # we want to pick a different answer that we haven't chosen previously.
+    while maxId == randomId:
+        randomId = random.randint(1, maxId)
+
+    # grab word of the day's id
+    c.execute("SELECT * FROM answers WHERE cur_word=1")
+    currentId = c.fetchone()[ID]
+
+    # Turn current word of the day "off"
+    c.execute("UPDATE answers SET cur_word=0 WHERE id=:id", {"id": currentId})
+
+    # Turn new word of the day "on"
+    c.execute("UPDATE answers SET cur_word=1 WHERE id=:id", {"id": randomId})
+    c.execute("SELECT * FROM answers WHERE cur_word=1")
+    answer_properties = c.fetchone()
+
+    # Changes word of the day
+    wordOTD = answer_properties[WORD]
+    return {"word": wordOTD}
--- db/convert_list_to_json.py
+++ db/convert_list_to_json.py
@@ -3,6 +3,7 @@
 
 import json
 import sys
+import random
 
 # using command-line argument to make code DRY
 filename = f"{sys.argv[1]}"
@@ -15,12 +16,23 @@ with open(in_file) as word_list:
     words = []  # will populate depending on cl-argument
     if filename == "answers":
         words = word_list.read().split(",")
-    for word in words or word_list:
-        obj = {}
-        obj["id"] = id
-        obj["name"] = "".join(ch for ch in word if ch.isalnum())
-        content.append(obj)
-        id = id + 1
+        for word in words:
+            obj = {}
+            obj["id"] = id
+            obj["name"] = "".join(ch for ch in word if ch.isalnum())
+            obj["cur_word"] = 0
+            content.append(obj)
+            id = id + 1
+        # all of the other words will have a propertie of "currentWord" that is 0 or 1
+        currentWordOfTheDay = random.randint(1, len(content))
+        content[currentWordOfTheDay]["cur_word"] = 1
+    elif filename == "word_list":
+        for word in word_list:
+            obj = {}
+            obj["id"] = id
+            obj["name"] = "".join(ch for ch in word if ch.isalnum())
+            content.append(obj)
+            id = id + 1
 
 # creating json file
 out = open(out_file, "w")
