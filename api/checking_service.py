3998eedbfde409ed2a4e190665759290012c13b9
super-rogatory <chukwudi10@gmail.com>
2022-04-08 18:23:58 -0700

    savepoint

--- api/checking_service.py
+++ api/checking_service.py
@@ -20,8 +20,8 @@ async def check_answer(answer: str):
     cw = await check_word(answer)
     if not cw["isValidWord"]:
         raise HTTPException(status_code=400, detail="Word is invalid.")
-    # Word is valid from this point on.
-    status = {1: "absent", 2: "present", 3: "correct"}
+    # Word is valid from this point on. | In production replace object with a small list [1,2,3]
+    statuses = {1: "absent", 2: "present", 3: "correct"}
     word_status = []  # list of objects. [{ "t", "present"}]
     index = 0
     isCorrect = False
@@ -30,10 +30,18 @@ async def check_answer(answer: str):
     else:
         for letter in answer:
             if letter == wordOTD[index]:
-                word_status.append({letter: status[3]})
+                word_status.append({letter: statuses[3]})
             elif wordOTD.find(letter) != -1:
-                word_status.append({letter, status[2]})
+                word_status.append({letter, statuses[2]})
             else:
-                word_status.append({letter, status[1]})
+                word_status.append({letter, statuses[1]})
             index = index + 1
     return {"answerResults": "Correct" if isCorrect else word_status}
+
+
+# @app.post("/checking/changeanswer"):
+# async def change_answer(answer: str):
+#     # use validation function to check if word is valid.
+#     cw = await check_word(answer)
+#     if not cw["isValidWord"]:
+#         raise HTTPException(status_code=400, detail="Word is invalid.")
