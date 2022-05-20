28a0c511d398fd0f1659b57f6b99c12c84cbf2fb
ryan <ryanbroguiere123@gmail.com>
2022-05-20 14:11:15 -0700

    /game/{game_id} WIP

--- api/app.py
+++ api/app.py
@@ -10,6 +10,13 @@ class NewGameInfo(BaseModel):
     username: str
 
 
+# defines a valid game in request body
+class Game(BaseModel):
+    game_id: int
+    finished: str
+    guesses: int
+    game_status: bool
+
 @app.post("/game/new")
 async def create_new_game(new_game: NewGameInfo):
     r = httpx.post(f"http://127.0.0.1:9999/api/statistics/new_game/{new_game.username}")
@@ -19,3 +26,10 @@ async def create_new_game(new_game: NewGameInfo):
 @app.get("/game/{id}")
 async def update_game(id: int):
     print(id)
+
+@app.post("/game/{game_id}")
+async def new_guess(guess: str, userid: int, game_status: Game):
+    #Verify that the guess is a word allowed by the dictionary
+    r = httpx.post(f"http://127.0.0.1:9999/api/validations/checkword/{guess}")
+    #Check that the user has guesses remaining
+    r = httpx.post(f"http://127.0.0.1:9999/api/statistics/game_result/{}")
