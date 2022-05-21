666e28854fd6a970de0018b8a23b4d812d76dc7a
super-rogatory <chukwudi10@gmail.com>
2022-05-20 20:56:58 -0700

    added guard clause to new_guess

--- api/app.py
+++ api/app.py
@@ -4,9 +4,7 @@ from datetime import date
 import httpx
 import uuid
 
-
 app = FastAPI()
-
 # defines a valid game in request body
 class NewGameInfo(BaseModel):
     username: str
@@ -35,7 +33,6 @@ async def create_new_game(new_game: NewGameInfo):
         )
         guesses_left = r.json()[f"game-{game_id}"]["guesses_left"]
         words_guessed = r.json()[f"game-{game_id}"]["words_guessed"]
-
         return {
             "status": res["status"],
             "user_id": user_id,
@@ -54,6 +51,13 @@ async def new_guess(game_id: int, new_guess: NewGuessInfo):
     # get current date
     today = date.today()
 
+    # check if user won already. if so do nothing
+    r = httpx.get(
+        f"http://127.0.0.1:9999/api/statistics/checkwin?user_id={new_guess.user_id}&game_id={game_id}"
+    )
+    if r.json()["status"] == True:
+        return "Thank you for playing Wordle! Come back tomorrow and play again! :)"
+
     # Verify that the guess is a word allowed by the dictionary
     # http://127.0.0.1:9999/api/validations/checkword?name=table
     # Make an API call to check word in order to see if the guess is valid
@@ -62,26 +66,35 @@ async def new_guess(game_id: int, new_guess: NewGuessInfo):
     )
     # extract boolean status from response
     is_valid_word = r.json()["isValidWord"]
-
     # Check that the user has guesses remaining
     # use REDIS services for tracking information. Get redis object.
     r = httpx.get(
         f"http://127.0.0.1:9999/api/trackings/get_status?user_id={new_guess.user_id}&game_id={game_id}"
     )
     guesses_left = r.json()[f"game-{game_id}"]["guesses_left"]
-
     # if word is invalid return error object
     if is_valid_word is not True:
         raise HTTPException(
             status_code=400, detail={"status": "invalid", "remaining": guesses_left}
         )
+    # Record the guess and update the number of guesses remaining
+    # create game_info object,for the body of post request
+    game_info = {"user_id": str(new_guess.user_id), "game_id": game_id}
+    # Using add_word function in tracking_service to handle recording the guess and returning number of guesses remaining
+    r = httpx.post(
+        f"http://127.0.0.1:9999/api/trackings/guess?guess={new_guess.guess}",
+        json=game_info,
+    )
+    # if user has no guesses left OR tries to play twice in a day.
+    if "detail" in r.json():
+        return "Thank you for playing Wordle! Come back tomorrow and play again! :)"
 
+    guesses_left = r.json()[f"game-{game_id}"]["guesses_left"]
     # Check to see if the guess is correct
     r = httpx.post(
         f"http://127.0.0.1:9999/api/checkings/checkanswer?answer={new_guess.guess}"
     )
     answer_res = r.json()["answerResults"]
-
     # TODO: if guesses_left is 0, save object as a LOSS, use the current date to update sql database, return sql object.
     # TODO: if guess is correct, save object as a WIN, use the current date to update sql database, return sql object.
     if answer_res == "Correct":
@@ -113,21 +126,7 @@ async def new_guess(game_id: int, new_guess: NewGuessInfo):
             "gamesWon": stats["gamesWon"],
             "averageGuesses": stats["averageGuesses"],
         }
-    # Record the guess and update the number of guesses remaining
-    # create game_info object,for the body of post request
-    game_info = {"user_id": str(new_guess.user_id), "game_id": game_id}
-    # Using add_word function in tracking_service to handle recording the guess and returning number of guesses remaining
-    r = httpx.post(
-        f"http://127.0.0.1:9999/api/trackings/guess?guess={new_guess.guess}",
-        json=game_info,
-    )
-    # if user has no guesses left OR tries to play twice in a day.
-    if "detail" in r.json():
-        return "Thank you for playing Wordle! Come back tomorrow and play again! :)"
-
-    guesses_left = r.json()[f"game-{game_id}"]["guesses_left"]
-
-    if guesses_left == 0:
+    elif guesses_left == 0:
         game_results = {
             "game_id": game_id,
             "finished": str(today),
